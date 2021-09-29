7d051a6520007d30a430e0173de0ab8e54389112
Dmitry Volodin <mr.molkree@gmail.com>
2021-09-29 23:54:36 +0300

    725. Split Linked List in Parts part 2 (dirty)

--- problems/725_split_linked_list_in_parts.py
+++ problems/725_split_linked_list_in_parts.py
@@ -11,9 +11,9 @@ class ListNode:
         self.next = next
 
 
-def to_list(node: Optional[ListNode]) -> list[int]:
+def to_list(node: Optional[ListNode]) -> Optional[list[int]]:
     if not node:
-        return []
+        return None
     result: list[int] = [node.val]
     node = node.next
     while node:
@@ -38,15 +38,42 @@ class Solution:
         self, head: Optional[ListNode], k: int
     ) -> list[Optional[ListNode]]:
         length = 0
-        while head:
+        copy_head = head
+        while copy_head:
             length += 1
-            head = head.next
-        return []
+            copy_head = copy_head.next
+        big_chunks = length % k
+        chunk_size = length // k
+        result: list[Optional[ListNode]] = []
+        copy_head = head
+        for _ in range(big_chunks):
+            result.append(copy_head)
+            last_node = copy_head
+            for _ in range(chunk_size):
+                last_node = last_node.next
+            copy_head = last_node.next
+            last_node.next = None
+        for _ in range(k - big_chunks):
+            result.append(copy_head)
+            if copy_head:
+                last_node = copy_head
+                for _ in range(chunk_size - 1):
+                    last_node = last_node.next
+                copy_head = last_node.next
+                last_node.next = None
+        return result
 
 
 solution = Solution()
 
 head = to_list_node([1, 2, 3])
 k = 5
+assert [[1], [2], [3], None, None] == [
+    to_list(node) for node in solution.splitListToParts(head, k)
+]
 
-assert [[1], [2], [3], None, None] == solution.splitListToParts(head, k)
+head = to_list_node([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+k = 3
+assert [[1, 2, 3, 4], [5, 6, 7], [8, 9, 10]] == [
+    to_list(node) for node in solution.splitListToParts(head, k)
+]
