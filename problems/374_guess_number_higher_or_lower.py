ada86bc68a8f1b688af85b02c5c897892685b9be
Dmitry Volodin <mr.molkree@gmail.com>
2021-10-12 18:37:23 +0300

    374. Guess Number Higher or Lower

---
+++ problems/374_guess_number_higher_or_lower.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/guess-number-higher-or-lower/
+# 374. Guess Number Higher or Lower
+
+
+def guess(num: int) -> int:
+    pass
+
+
+class Solution:
+    def guessNumber(self, n: int) -> int:  # noqa: N802
+        pick = n // 2
+        lower = 1
+        upper = n
+        next_guess: int = guess(pick)
+        while next_guess:
+            if next_guess < 0:
+                upper = pick - 1
+            else:
+                lower = pick + 1
+            pick = (upper + lower) // 2
+            next_guess = guess(pick)
+        return pick
