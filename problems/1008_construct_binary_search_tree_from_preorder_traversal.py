ae4b4d52e1e36d738cc5627d7c1ae359283aab36
Dmitry Volodin <mr.molkree@gmail.com>
2021-10-13 21:47:50 +0300

    1008. Construct Binary Search Tree from Preorder Traversal

---
+++ problems/1008_construct_binary_search_tree_from_preorder_traversal.py
@@ -0,0 +1,34 @@
+# https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+# 1008. Construct Binary Search Tree from Preorder Traversal
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val: int = 0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def build_bst(
+        self, min: int, max: int, preorder: list[int], index: int
+    ) -> tuple[TreeNode | None, int]:
+        if index == len(preorder):
+            return None, index
+        value = preorder[index]
+        if value < min or value > max:
+            return None, index
+        root = TreeNode(value)
+        index += 1
+        root.left, index = self.build_bst(min, value - 1, preorder, index)
+        root.right, index = self.build_bst(value + 1, max, preorder, index)
+        return root, index
+
+    def bstFromPreorder(self, preorder: list[int]) -> TreeNode | None:  # noqa: N802
+        return self.build_bst(0, 10 ** 8 + 1, preorder, 0)[0]
+
+
+solution = Solution()
+
+solution.bstFromPreorder([8, 5, 1, 7, 10, 12])
