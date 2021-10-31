a482d6e94965f8ec19b4e514fd46313c52491483
Dmitry Volodin <mr.molkree@gmail.com>
2021-10-31 17:00:05 +0300

    430. Flatten a Multilevel Doubly Linked List

---
+++ problems/430_flatten_a_multilevel_doubly_linked_list.py
@@ -0,0 +1,54 @@
+# https://leetcode.com/problems/flatten-a-multilevel-doubly-linked-list/
+# 430. Flatten a Multilevel Doubly Linked List
+
+
+class Node:
+    def __init__(self, val: int, prev, next, child):
+        self.val = val
+        self.prev = prev
+        self.next = next
+        self.child = child
+
+
+class Solution:
+    def flatten_children(self, head: Node, next_node: Node) -> Node:
+        node = head
+        while node.next:
+            if node.child:
+                node.next = self.flatten_children(node.child, node.next)
+                node.next.prev = node
+                node.child = None
+            node = node.next
+        node.next = next_node
+        if next_node:
+            next_node.prev = node
+        return head
+
+    def flatten(self, head: Node) -> Node:
+        node = head
+        while node:
+            if node.child:
+                node.next = self.flatten_children(node.child, node.next)
+                node.next.prev = node
+                node.child = None
+            node = node.next
+        return head
+
+
+solution = Solution()
+
+
+head = Node(1, None, None, None)
+head.child = Node(3, None, None, None)
+node = Node(2, head, None, None)
+head.next = node
+head = solution.flatten(head)
+while head:
+    print(head.val)
+    head = head.next
+
+head = Node(1, None, None, Node(2, None, None, Node(3, None, None, None)))
+head = solution.flatten(head)
+while head:
+    print(head.val)
+    head = head.next
