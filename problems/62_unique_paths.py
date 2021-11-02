f64e09587d500800e48aaf308c004ea3eaf96ab8
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-02 20:47:24 +0300

    62. Simplify solution

--- problems/62_unique_paths.py
+++ problems/62_unique_paths.py
@@ -4,15 +4,11 @@
 
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:  # noqa: N802
-        paths = [[0] * n] * m
-        for i in range(m):
-            paths[i][0] = 1
-        for j in range(n):
-            paths[0][j] = 1
-        for i in range(1, m):
+        row = [1] * n
+        for _ in range(1, m):
             for j in range(1, n):
-                paths[i][j] = paths[i - 1][j] + paths[i][j - 1]
-        return paths[-1][-1]
+                row[j] += row[j - 1]
+        return row[-1]
 
 
 solution = Solution()
