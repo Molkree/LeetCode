abe73516d37b17886c8b946ee59346fbaa5bbccb
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-03 19:30:56 +0300

    129. Sum Root to Leaf Numbers

---
+++ problems/129_sum_root_to_leaf_numbers.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/sum-root-to-leaf-numbers/
+# 129. Sum Root to Leaf Numbers
+
+
+class TreeNode:
+    def __init__(self, val: int = 0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def sumNumbers(self, root: TreeNode) -> int:  # noqa: N802
+        sum = 0
+
+        def dfs(root: TreeNode, num: int):
+            if not root:
+                return
+            num = num * 10 + root.val
+            if not root.left and not root.right:
+                nonlocal sum
+                sum += num
+                return
+            dfs(root.left, num)
+            dfs(root.right, num)
+
+        dfs(root, 0)
+        return sum
+
+
+solution = Solution()
+
+
+root = TreeNode(1, TreeNode(2), TreeNode(3))
+assert solution.sumNumbers(root) == 25
+
+root = TreeNode(4, TreeNode(9, TreeNode(5), TreeNode(1)), TreeNode(0))
+assert solution.sumNumbers(root) == 1026
+
+root = TreeNode(0, TreeNode(1))
+assert solution.sumNumbers(root) == 1
