a769b69746783ea3ea87fa2a5b05d23e91cefb69
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-05 16:39:30 +0300

    441. Math solution

--- problems/441_arranging_coins.py
+++ problems/441_arranging_coins.py
@@ -2,13 +2,19 @@
 # 441. Arranging Coins
 
 
+from math import sqrt
+
+
 class Solution:
     def arrangeCoins(self, n: int) -> int:  # noqa: N802
-        coins_per_row = 1
-        while n > 0:
-            coins_per_row += 1
-            n -= coins_per_row
-        return coins_per_row - 1
+        # iterative
+        # coins_per_row = 1
+        # while n > 0:
+        #     coins_per_row += 1
+        #     n -= coins_per_row
+        # return coins_per_row - 1
+        # math
+        return int(sqrt(2 * n + 0.25) - 0.5)
 
 
 solution = Solution()
