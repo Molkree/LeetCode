9d41ceda9e969e0370d48cadbd92601adf6d2bb0
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-10 20:33:02 +0300

    121. Best Time to Buy and Sell Stock

---
+++ problems/121_best_time_to_buy_and_sell_stock.py
@@ -0,0 +1,22 @@
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+# 121. Best Time to Buy and Sell Stock
+
+
+class Solution:
+    def maxProfit(self, prices: list[int]) -> int:  # noqa: N802
+        buy = 10 ** 5 + 1
+        sell = 0
+        for price in prices:
+            buy = min(buy, price)
+            sell = max(sell, price - buy)
+        return sell
+
+
+solution = Solution()
+
+
+prices = [7, 1, 5, 3, 6, 4]
+assert solution.maxProfit(prices) == 5
+
+prices = [7, 6, 4, 3, 1]
+assert solution.maxProfit(prices) == 0
