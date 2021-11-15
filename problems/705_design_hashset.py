854974e65c003a0377aeb09dc257454bd49424db
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-15 18:40:18 +0300

    705. Design HashSet
    This is an "Easy" question so it gets this "easy" solution

---
+++ problems/705_design_hashset.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/design-hashset/
+# 705. Design HashSet
+
+
+class MyHashSet:
+    def __init__(self) -> None:
+        self.buckets = [False] * 1000000
+
+    def add(self, key: int) -> None:
+        self.buckets[key] = True
+
+    def remove(self, key: int) -> None:
+        self.buckets[key] = False
+
+    def contains(self, key: int) -> bool:
+        return self.buckets[key]
+
+
+my_hash_set = MyHashSet()
+my_hash_set.add(1)
+my_hash_set.add(2)
+assert my_hash_set.contains(1)
+assert not my_hash_set.contains(3)
+my_hash_set.add(2)
+assert my_hash_set.contains(2)
+my_hash_set.remove(2)
+assert not my_hash_set.contains(2)
