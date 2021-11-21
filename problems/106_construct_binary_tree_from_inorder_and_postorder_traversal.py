e2d4f46b0b850b862f50e06515716d1aa6c7a7e4
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-22 00:52:39 +0300

    106. Construct Binary Tree from Inorder and Postorder Traversal

---
+++ problems/106_construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+# 106. Construct Binary Tree from Inorder and Postorder Traversal
+
+
+class TreeNode:
+    def __init__(self, val: int = 0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def buildTree(  # noqa: N802
+        self, inorder: list[int], postorder: list[int]
+    ) -> TreeNode | None:
+        inorder_indices = {value: index for index, value in enumerate(inorder)}
+
+        def build_subtree(low: int, high: int) -> TreeNode | None:
+            if low > high:
+                return None
+            root = TreeNode(postorder.pop())
+            root_index = inorder_indices[root.val]
+            root.right = build_subtree(root_index + 1, high)
+            root.left = build_subtree(low, root_index - 1)
+            return root
+
+        return build_subtree(0, len(inorder) - 1)
