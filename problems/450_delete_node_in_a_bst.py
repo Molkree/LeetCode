d707a42fa36d4319f5cd786951baadba330757ce
Dmitry Volodin <mr.molkree@gmail.com>
2021-11-23 02:20:07 +0300

    450. Delete Node in a BST

---
+++ problems/450_delete_node_in_a_bst.py
@@ -0,0 +1,33 @@
+# https://leetcode.com/problems/delete-node-in-a-bst/
+# 450. Delete Node in a BST
+
+
+class TreeNode:
+    def __init__(self, val: int = 0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def deleteNode(  # noqa: N802
+        self, root: TreeNode | None, key: int
+    ) -> TreeNode | None:
+        if not root:
+            return None
+        if key < root.val:
+            root.left = self.deleteNode(root.left, key)
+        elif root.val < key:
+            root.right = self.deleteNode(root.right, key)
+        else:
+            if not root.right:
+                return root.left
+            if not root.left:
+                return root.right
+            node = root.right
+            while node.left:
+                node = node.left
+            root.val = node.val
+            root.right = self.deleteNode(root.right, root.val)
+
+        return root
