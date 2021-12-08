9d999dd4306ce7dd45ade9bff426e5ce059acdc7
Dmitry Volodin <mr.molkree@gmail.com>
2021-12-08 18:50:14 +0300

    563. Binary Tree Tilt

---
+++ problems/563_binary_tree_tilt.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/binary-tree-tilt/
+# 563. Binary Tree Tilt
+
+
+from __future__ import annotations
+
+
+class TreeNode:
+    def __init__(
+        self, val: int = 0, left: TreeNode | None = None, right: TreeNode | None = None
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def findTilt(self, root: TreeNode | None) -> int:  # noqa: N802
+        tilt = 0
+
+        def node_sum(root: TreeNode | None) -> int:
+            nonlocal tilt
+            if not root:
+                return 0
+            left_sum = node_sum(root.left)
+            right_sum = node_sum(root.right)
+            tilt += abs(left_sum - right_sum)
+            return left_sum + right_sum + root.val
+
+        node_sum(root)
+        return tilt
