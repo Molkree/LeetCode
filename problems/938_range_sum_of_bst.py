6019206e4f92b666e8e6e1de05c9ef114c0e7679
Dmitry Volodin <mr.molkree@gmail.com>
2021-12-14 22:33:47 +0300

    938. Range Sum of BST

---
+++ problems/938_range_sum_of_bst.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/range-sum-of-bst/
+# 938. Range Sum of BST
+
+
+from __future__ import annotations
+
+
+class TreeNode:
+    def __init__(
+        self, val: int = 0, left: TreeNode | None = None, right: TreeNode | None = None
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def rangeSumBST(  # noqa: N802
+        self, root: TreeNode | None, low: int, high: int
+    ) -> int:
+        if not root:
+            return 0
+        if root.val > high:
+            return self.rangeSumBST(root.left, low, high)
+        if root.val < low:
+            return self.rangeSumBST(root.right, low, high)
+        return (
+            root.val
+            + self.rangeSumBST(root.left, low, high)
+            + self.rangeSumBST(root.right, low, high)
+        )
