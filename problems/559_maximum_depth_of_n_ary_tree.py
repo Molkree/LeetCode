6115227b6bda6895da87bacc3529a0c2cfdb7951
Dmitry Volodin <mr.molkree@gmail.com>
2021-12-16 21:27:00 +0300

    559. Maximum Depth of N-ary Tree

---
+++ problems/559_maximum_depth_of_n_ary_tree.py
@@ -0,0 +1,38 @@
+# https://leetcode.com/problems/maximum-depth-of-n-ary-tree/
+# 559. Maximum Depth of N-ary Tree
+
+
+from __future__ import annotations
+
+
+class Node:
+    def __init__(self, val: int, children: list[Node]):
+        self.val = val
+        self.children = children
+
+
+class Solution:
+    def maxDepth(self, root: Node | None) -> int:  # noqa: N802
+        # Slow version
+        # max_depth = 1
+        # stack: list[tuple[Node, int]] = [(root, 1)]
+        # while stack:
+        #     node, depth = stack.pop()
+        #     max_depth = max(max_depth, depth)
+        #     if node.children:
+        #         stack += [(child, depth + 1) for child in node.children]
+        # return max_depth
+
+        # Faster version
+        if not root:
+            return 0
+
+        depths: list[int] = []
+
+        def dfs(root: Node, depth: int) -> None:
+            for child in root.children:
+                dfs(child, depth + 1)
+            depths.append(depth)
+
+        dfs(root, 1)
+        return max(depths)
