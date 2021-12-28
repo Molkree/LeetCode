d12182c955ceef8eaf89e52ee56fcf2a5f81bdf4
Dmitry Volodin <mr.molkree@gmail.com>
2021-12-28 19:25:53 +0300

    876. Middle of the Linked List

---
+++ problems/876_middle_of_the_linked_list.py
@@ -0,0 +1,20 @@
+# https://leetcode.com/problems/middle-of-the-linked-list/
+# 876. Middle of the Linked List
+
+
+from __future__ import annotations
+
+
+class ListNode:
+    def __init__(self, val: int = 0, next: ListNode | None = None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def middleNode(self, head: ListNode | None) -> ListNode | None:  # noqa: N802
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next  # type: ignore
+            fast = fast.next.next
+        return slow
