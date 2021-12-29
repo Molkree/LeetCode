b8f1928158897c5fb599da4e2a06e7197ca42184
Dmitry Volodin <mr.molkree@gmail.com>
2021-12-29 21:12:08 +0300

    116. Populating Next Right Pointers in Each Node

---
+++ problems/116_populating_next_right_pointers_in_each_node.py
@@ -0,0 +1,36 @@
+# https://leetcode.com/problems/populating-next-right-pointers-in-each-node/
+# 116. Populating Next Right Pointers in Each Node
+
+
+from __future__ import annotations
+
+
+class Node:
+    def __init__(
+        self,
+        val: int = 0,
+        left: Node | None = None,
+        right: Node | None = None,
+        next: Node | None = None,
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+
+
+class Solution:
+    def connect(self, root: Node | None) -> Node | None:
+        if not root:
+            return root
+        prev = root
+        cur: Node | None = None
+        while prev.left:
+            cur = prev
+            while cur:
+                cur.left.next = cur.right  # type: ignore
+                if cur.next:
+                    cur.right.next = cur.next.left  # type: ignore
+                cur = cur.next
+            prev = prev.left
+        return root
