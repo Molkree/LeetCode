80fd21dfc8cd77acb7a0d53b130e3c5ff3c12c02
Dmitry Volodin <mr.molkree@gmail.com>
2022-01-12 21:25:57 +0300

    701. Insert into a Binary Search Tree

---
+++ problems/701_insert_into_a_binary_search_tree.py
@@ -0,0 +1,35 @@
+# https://leetcode.com/problems/insert-into-a-binary-search-tree/
+# 701. Insert into a Binary Search Tree
+
+
+from __future__ import annotations
+
+
+class TreeNode:
+    def __init__(
+        self, val: int = 0, left: TreeNode | None = None, right: TreeNode | None = None
+    ):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def insertIntoBST(  # noqa: N802
+        self, root: TreeNode | None, val: int
+    ) -> TreeNode | None:
+        if not root:
+            return TreeNode(val)
+        node = root
+        while node.left or node.right:
+            if node.val > val and node.left:
+                node = node.left
+            elif node.val < val and node.right:
+                node = node.right
+            else:
+                break
+        if node.val > val:
+            node.left = TreeNode(val)
+        else:
+            node.right = TreeNode(val)
+        return root
