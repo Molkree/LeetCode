03f85d0395ff75afef6544f1e568b080980cacbe
Dmitry Volodin <mr.molkree@gmail.com>
2022-03-04 00:10:45 +0300

    413. Simplify

--- problems/413_arithmetic_slices.py
+++ problems/413_arithmetic_slices.py
@@ -4,24 +4,14 @@
 
 class Solution:
     def numberOfArithmeticSlices(self, nums: list[int]) -> int:  # noqa: N802
-        if len(nums) < 3:
-            return 0
         count = 0
-        length = 2
-
-        def count_arrays(length: int) -> int:
-            n = length - 2
-            a_1 = 1
-            return int(n / 2 * (2 * a_1 + (n - 1)))
-
+        arrays = 0
         for i in range(1, len(nums) - 1):
-            if nums[i] - nums[i + 1] == nums[i - 1] - nums[i]:
-                length += 1
+            if nums[i - 1] - nums[i] == nums[i] - nums[i + 1]:
+                arrays += 1
+                count += arrays
             else:
-                count += count_arrays(length)
-                length = 2
-        if length > 2:
-            count += count_arrays(length)
+                arrays = 0
         return count
 
 
