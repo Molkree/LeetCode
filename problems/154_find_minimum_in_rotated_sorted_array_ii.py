dd6925629d32738ad63931381d3ae0f94c45dc36
Dmitry Volodin <mr.molkree@gmail.com>
2022-04-08 21:45:57 +0400

    154. Tweak

--- problems/154_find_minimum_in_rotated_sorted_array_ii.py
+++ problems/154_find_minimum_in_rotated_sorted_array_ii.py
@@ -7,7 +7,7 @@ class Solution:
         low = 0
         high = len(nums) - 1
         while low < high:
-            mid = low + (high - low) // 2
+            mid = (low + high) // 2
             if nums[mid] > nums[high]:
                 low = mid + 1
             elif nums[mid] < nums[high]:
