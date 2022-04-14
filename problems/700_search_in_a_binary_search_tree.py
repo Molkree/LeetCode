9ba6101dc38056a9d74c8ce09bba037a9e4c6130
Dmitry Volodin <mr.molkree@gmail.com>
2022-04-15 03:00:08 +0400

    700. Search in a Binary Search Tree

---
+++ problems/700_search_in_a_binary_search_tree.py
@@ -0,0 +1,27 @@
+# https://leetcode.com/problems/search-in-a-binary-search-tree/
+# 700. Search in a Binary Search Tree
+
+
+from __future__ import annotations
+
+
+class TreeNode:
+    def __init__(
+        self, val: int = 0, left: TreeNode | None = None, right: TreeNode | None = None
+    ) -> None:
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+    def searchBST(  # noqa: N802
+        self, root: TreeNode | None, val: int
+    ) -> TreeNode | None:
+        if not root:
+            return None
+        if val < root.val:
+            return self.searchBST(root.left, val)
+        if val > root.val:
+            return self.searchBST(root.right, val)
+        return root
