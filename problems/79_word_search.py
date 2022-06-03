6398d15eb1c7f7087272a029ede747e8fb8d1634
Dmitry Volodin <mr.molkree@gmail.com>
2022-06-03 18:14:21 +0400

    79. Small rewrite

--- problems/79_word_search.py
+++ problems/79_word_search.py
@@ -3,38 +3,35 @@
 
 
 class Solution:
-    def word_starts_here(
-        self,
-        board: list[list[str]],
-        word: str,
-        i: int,
-        j: int,
-        visited: set[tuple[int, int]],
-    ) -> bool:
-        if not word:
-            return True
-        height = len(board)
-        width = len(board[0])
-        if i < 0 or i > height - 1 or j < 0 or j > width - 1 or (i, j) in visited:
-            return False
-        if board[i][j] == word[0]:
-            visited.add((i, j))
-            if (
-                self.word_starts_here(board, word[1:], i - 1, j, visited)
-                or self.word_starts_here(board, word[1:], i + 1, j, visited)
-                or self.word_starts_here(board, word[1:], i, j - 1, visited)
-                or self.word_starts_here(board, word[1:], i, j + 1, visited)
-            ):
-                return True
-            visited.remove((i, j))
-        return False
-
     def exist(self, board: list[list[str]], word: str) -> bool:
         height = len(board)
         width = len(board[0])
+
+        def word_starts_here(
+            word: str,
+            i: int,
+            j: int,
+            visited: set[tuple[int, int]],
+        ) -> bool:
+            if not word:
+                return True
+            if not 0 <= i <= height - 1 or not 0 <= j <= width - 1 or (i, j) in visited:
+                return False
+            if board[i][j] == word[0]:
+                visited.add((i, j))
+                if (
+                    word_starts_here(word[1:], i - 1, j, visited)
+                    or word_starts_here(word[1:], i + 1, j, visited)
+                    or word_starts_here(word[1:], i, j - 1, visited)
+                    or word_starts_here(word[1:], i, j + 1, visited)
+                ):
+                    return True
+                visited.remove((i, j))
+            return False
+
         for i in range(height):
             for j in range(width):
-                if self.word_starts_here(board, word, i, j, set()):
+                if word_starts_here(word, i, j, set()):
                     return True
         return False
 
