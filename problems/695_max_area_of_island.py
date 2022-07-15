a94d8aba7db856e1a12a815fcf22e6211875dfdc
Dmitry Volodin <mr.molkree@gmail.com>
2022-07-16 01:47:18 +0400

    695. Max Area of Island

---
+++ problems/695_max_area_of_island.py
@@ -0,0 +1,39 @@
+# https://leetcode.com/problems/max-area-of-island/
+# 695. Max Area of Island
+
+
+class Solution:
+    def maxAreaOfIsland(self, grid: list[list[int]]) -> int:  # noqa: N802
+        max_area = 0
+
+        def dfs(i: int, j: int) -> int:
+            if i < 0 or i >= len(grid) or j < 0 or j >= len(grid[0]) or grid[i][j] == 0:
+                return 0
+            grid[i][j] = 0
+            return 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1)
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 1:
+                    max_area = max(max_area, dfs(i, j))
+
+        return max_area
+
+
+solution = Solution()
+
+
+grid = [
+    [0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0],
+]
+assert solution.maxAreaOfIsland(grid) == 6
+
+grid = [[0, 0, 0, 0, 0, 0, 0, 0]]
+assert solution.maxAreaOfIsland(grid) == 0
