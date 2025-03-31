427a0c8bdab4e5c015a5429a4c108b7033e84c1f
Dmitry Volodin <mr.molkree@gmail.com>
2025-04-01 02:29:20 +0400

    refactor(131): remove nonlocal usage

--- problems/131_palindrome_partitioning.py
+++ problems/131_palindrome_partitioning.py
@@ -7,23 +7,24 @@ from functools import cache
 
 class Solution:
     def partition(self, s: str) -> list[list[str]]:
-        result = list[list[str]]()
+        result: list[list[str]] = []
 
         @cache
         def is_palindrome(s: str) -> bool:
             return s == s[::-1]
 
-        def dfs(start: int, current_list: list[str]) -> None:
-            nonlocal s, result
+        def dfs(
+            s: str, start: int, current_list: list[str], result: list[list[str]]
+        ) -> None:
             if start == len(s):
                 result += [list(current_list)]
             for end in range(start, len(s)):
                 if is_palindrome(s[start : end + 1]):
                     current_list.append(s[start : end + 1])
-                    dfs(end + 1, current_list)
+                    dfs(s, end + 1, current_list, result)
                     current_list.pop()
 
-        dfs(0, [])
+        dfs(s, 0, [], result)
         return result
 
 
